332c3bc24978dce19d30f0840ffd4bdc133cb652
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-11 19:42:23 -0700

    Finished the PhonePurchase.js program. May add more features later

--- PhonePurchase.js
+++ PhonePurchase.js
@@ -1,33 +1,60 @@
-console.log("You have $900");
+const TAX_RATE = .08;
+const PHONE_PRICE = 99.99;
+const ACC_PRICE = 9.99;
 
-var phones = ["iPhone 7+", "Galaxy S8", "Google Pixel"];
-var phonePrice = ["$150", "$175", "$300"];
-var acc = ["Phone Case", "Charger", "Screen Protector"];
-var accPrice = ["$35", "$50", "$50"];
+var bank_balance = 333.33;
+var amount = 0;
 
-printPhoneInfo();
-// Write the array
-phones.forEach((item) => {
-    console.log(item);
-});
-
-
-function printPhoneInfo()
-{
-  console.log("Here are your phone options:")
-  console.log("Phone\t\t" + "Price");
-
-  for(var i = 0; i < phones.length; i++)
-  {
-    console.log(phones[i] + "\t" + phonePrice[i]);
-  }
+function calcTax(amount) {
+    amount += amount * .08;
+    return amount;
 }
 
+function printPrice(price) {
+  return "$" + price.toFixed(2);
+}
 
+console.log("Bank Balance = " + printPrice(bank_balance));
 
+var i = 1;
+while (bank_balance > PHONE_PRICE) {
 
+   // Show what number of phone purchase they are on
+    console.log("Buying phone number: " + i);
 
-/*while(bankAccount > 0)
-{
+    // Show the price of the phone
+    console.log("Price = " + printPrice(PHONE_PRICE));
 
-}*/
+    // Get and show the price of the phone after tax
+    var priceAfterTax = calcTax(PHONE_PRICE);
+    console.log("Price after Tax = " + printPrice(priceAfterTax));
+
+    // Minus purchase from bank account
+    bank_balance -= priceAfterTax;
+
+    if (bank_balance > 0) {
+        console.log("New Balance = " + bank_balance.toFixed(2));
+    } else {
+        console.log("You can't afford phone number: " + i);
+    }
+
+    if(bank_balance > ACC_PRICE)
+    {
+      console.log("\n\tBuying Accessory");
+
+      // Show the price of the Accessory
+      console.log("\tPrice = " + printPrice(ACC_PRICE));
+
+      // Get and show the price of the phone after tax
+      var accAfterTax = calcTax(ACC_PRICE);
+      console.log("\tPrice after Tax = " + printPrice(accAfterTax));
+
+      // Minus purchase from bank account
+      bank_balance -= accAfterTax;
+      console.log("\tNew Balance = " + bank_balance.toFixed(2));
+    }
+    console.log("");
+    i++;
+}
+
+console.log("You have " + printPrice(bank_balance) + " left, not enough for another phone");
