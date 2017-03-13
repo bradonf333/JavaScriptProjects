07bad909be36d6c2c2862a97460007600d942e8e
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-12 19:30:00 -0600

    Added an array and a function to loop through array.

--- FunctionPractice.js
+++ FunctionPractice.js
@@ -1,3 +1,6 @@
+// An Array of friends
+var friends = ["Mark", "Lisa", "Danny"];
+
 // Get the area of a shape with 2 sides
 function calculateArea(side1, side2) {
     return side1 * side2;
@@ -8,5 +11,24 @@ function randomNumber() {
     return Math.random();
 }
 
+// Function to easily loop through friends array
+function greetFriends() {
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i]);
+    }
+    console.log();
+}
+
 console.log(calculateArea(5, 50));
 console.log(randomNumber());
+console.log();
+
+// Array Stuff
+console.log("Full Array:")
+greetFriends();
+friends.push("Chuck");
+console.log("Push new friend Chuck to the array");
+greetFriends();
+console.log("No longer friends with Chuck, pop him off the list");
+friends.pop();
+greetFriends();
