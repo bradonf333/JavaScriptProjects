0533eb6c9a5b4e2a7d81f04dbeedd389cabd431f
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-12 19:28:09 -0600

    Added FunctionPractice.js, using to practice functions.

---
+++ FunctionPractice.js
@@ -0,0 +1,12 @@
+// Get the area of a shape with 2 sides
+function calculateArea(side1, side2) {
+    return side1 * side2;
+}
+
+// Get a random number
+function randomNumber() {
+    return Math.random();
+}
+
+console.log(calculateArea(5, 50));
+console.log(randomNumber());
---
+++ README.md
@@ -0,0 +1,3 @@
+These are some JavaScript apps I have started practicing on.
+
+There will be alot of random js scripts in here. If they are related I might start putting them into different folders.
