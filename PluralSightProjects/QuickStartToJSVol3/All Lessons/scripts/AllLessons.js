0f39fbf98a5824e909dfaebb8fa5af5295a7f6ad
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-15 18:21:05 -0600

    Added lesson 7 Global vs Local

--- PluralSightProjects/QuickStartToJSVol3/All Lessons/scripts/AllLessons.js
+++ PluralSightProjects/QuickStartToJSVol3/All Lessons/scripts/AllLessons.js
@@ -83,3 +83,23 @@ function alert(){
 }
 alert();
 */
+
+/*----------------------------
+        -- Lesson 7 --
+    
+    -- Local & Global Scope    
+ -----------------------------*/
+var example = "Example";
+
+function alertExample(){
+    var anotherExample = "Example inside Function";
+    globalExample = "This is bad!";
+}
+
+// anotherExample is unreachable because it was declared in the alertExample function
+alert(anotherExample);
+
+// globalExample is reachable here because in the alertExample function we did not use the var keywor
+// This is very bad and should never be done in practice.
+alert(globalExample);
+
